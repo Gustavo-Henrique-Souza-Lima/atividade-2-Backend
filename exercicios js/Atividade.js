14cb6d56586f414c245ca07dbb65ec9b3e8461f0
Gustavo Lima <168701466+Gustavo-Henrique-Souza-Lima@users.noreply.github.com>
2024-08-21 22:35:25 -0300

    Primeiro commit
    Commit com as atividades de 1 a 15

---
+++ exercicios js/Atividade.js
@@ -0,0 +1,141 @@
+// Atividade 1
+// const texto = 'doutor, eu não me engano, meu coração é corinthiano';
+
+// console.log('Quantidade letras do texto: ',texto.length,'Texto sem estar no aumentativo: ',texto,'Texto no auemntativo: ',texto.toUpperCase()) 
+
+// Atividade 2
+// const valornull = null;
+// const semvalor = undefined;
+// console.log(valornull, semvalor)
+
+// Atividade 3
+// const nome = 'Gislene'
+// const nota1 = 6;
+// const nota2 = 4;
+// const nota3 = 8;
+// const total = nota1 + nota2 + nota3 / 3;
+// if (total > 5 === true){
+//     console.log(nome,', você passou com a nota: ', total)
+// }else{
+//     console.log(nome, 'infelizmente você foi reprovada')
+// }
+
+// Atividade 5
+// const texto1 = 'AlO, pEsSoAl';
+// const texto2 = 'VaI CoRiNtHiAnS';
+
+// if (texto1.length > 7 || texto2.length > 7){
+//     console.log('A quantidade letras neste texto é maior que 7:', texto1.toUpperCase(), texto2.toUpperCase());
+// }else{
+//     console.log('A quantidade letras neste texto é menor que 7: ',texto1.toLowerCase(), texto2.toLowerCase());
+// }
+
+// Atividade 6
+// const saldoAnt = 1000;
+// const deposito = saldoAnt + 1000;
+// const saldoAtual = deposito;
+// if (saldoAtual >= 1500){
+//     const mensagem = 'Conta bancaria positiva';
+//     const compra = saldoAtual - 1200;
+//     const saldoDpsCompra = compra;
+//     console.log('A compra foi aprovada e valor que você ficou depois da compra foi de: R$',saldoDpsCompra, '(',mensagem,')')
+
+// }else{
+//     console.log('Compra não aprovada pois o valor é abaixo de 1500!')
+// }
+
+// Atividade 7
+// const valor = 10
+// const a = valor%2==0?"Par":"Ímpar"
+// console.log('O valor é:', a)
+
+// Atividade 8
+// const permissão = true
+// const login = true
+// if (permissão && login === true){
+//     console.log('Você está logado mas tem permissão')
+// }else{
+//     console.log('Você não está logado e não tem permissão')
+// }
+
+// Atividade 9
+// const red = true
+// const blue = false
+// if (red || blue === true){
+//     console.log('Uma das cores é verdadeira')
+// }else{
+//     console.log('As duas cores são falsas')
+// }
+
+// Atividade 10
+// const idade = 15;
+// const idadeMinima = 18;
+
+// if (idade >= idadeMinima) {
+//     console.log('Idade mínima é 18 e você tem:', idade);
+// } else {
+//     console.log('Você não tem a idade permitida. Idade atual:', idade);
+// }
+// Exercício 11
+ 
+/*var nome = "Gabriel"
+ 
+function saudacao (){
+    console.log("Saudação meu amigo/a",nome)
+}
+ 
+saudacao() */
+ 
+// Exercício 12
+ 
+/*
+var idade = 20
+ 
+function deMaior (idade){
+    if (idade>= 18){
+        return console.log("É de maior")
+    }else{
+        return console.log("É de menor")
+    }
+}
+ 
+deMaior(idade) */
+ 
+// Exercício 13
+ 
+/* texto = "radar"
+function palindromo (){
+    if (texto === texto.split("").reverse().join("")){
+        console.log("É palindromo")
+    }else{
+        console.log("Não é palindromo")
+    }
+}
+palindromo()  */
+ 
+// Exercício 14
+ 
+/* function maior(a,b,c){
+    if (a>b && a>c){
+      return  console.log(a,"é o maior número")
+    }
+    else if (b>c && b>a){
+      return  console.log(b,"é o maior número")
+    }
+    else if (c>a && c>b){
+     return   console.log(c,"é o maior número")
+    }
+    else{
+     return   console.log("Temos números com valores iguais")
+    }
+}
+ 
+maior(5,8,3) */
+ 
+// Exercício 15
+/* function calculaPotencia(base, expoente){
+ return console.log(base,"elevado a",expoente,"é igual a:" ,base**expoente)
+}
+ 
+calculaPotencia(4,3)  */
+ 
